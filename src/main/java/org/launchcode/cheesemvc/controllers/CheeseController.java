0fd4ddffa8d9277ef56bf2a9086e68726bb2e808
David Lawrie <dlawrie@att.net>
2019-03-10 15:37:49 -0500

    Added remove feature

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -15,25 +15,44 @@ public class CheeseController {
 
     // Request path: /cheese
     @RequestMapping(value = "")
-    public String index(Model model){
+    public String index(Model model) {
 
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
 
-    @RequestMapping(value="add", method = RequestMethod.GET)
-    public static String displayAddCheeseForm(Model model){
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public static String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
-    @RequestMapping(value="add", method = RequestMethod.POST)
-    public static String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc){
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public static String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc) {
 
         cheeses.put(cheeseName, cheeseDesc);
 
         //redirect to /cheese
         return "redirect:";
     }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public static String displayRemoveCheeseForm(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/remove";
+    }
+
+    //Request path: /cheese/remove
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public static String processRemoveCheeseForm(@RequestParam String removeCheese) {
+
+       cheeses.remove(removeCheese, cheeses.get(removeCheese));
+        return "redirect:";
+    }
 }
+
+
