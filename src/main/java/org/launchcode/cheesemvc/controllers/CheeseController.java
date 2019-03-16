4cdb17fbbe80d796e36a68c32d76ac7f19de7410
David Lawrie <dlawrie@att.net>
2019-03-15 21:15:44 -0500

    Implemented Cheese class

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,17 +1,20 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -28,10 +31,12 @@ public class CheeseController {
         return "cheese/add";
     }
 
+    // request path: cheese/add
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public static String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc) {
 
-        cheeses.put(cheeseName, cheeseDesc);
+        Cheese cheese = new Cheese(cheeseName, cheeseDesc);
+        cheeses.add(cheese);
 
         //redirect to /cheese
         return "redirect:";
@@ -50,7 +55,22 @@ public class CheeseController {
     @RequestMapping(value = "remove", method = RequestMethod.POST)
     public static String processRemoveCheeseForm(@RequestParam String removeCheese) {
 
-       cheeses.remove(removeCheese, cheeses.get(removeCheese));
+        if (removeCheese.length() < 1) {
+            //System.out.println("empty");
+            return "redirect:";
+        }
+
+        for (Cheese cheese : cheeses) {
+            if (cheese.getName().equals(removeCheese)) {
+                cheeses.remove(cheese);
+                return "redirect:";
+            }
+
+        }
+
+        if (cheeses.size() < 1) {
+            return "redirect:";
+        }
         return "redirect:";
     }
 }
